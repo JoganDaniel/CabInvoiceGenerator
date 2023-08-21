1f9664bebad89f3560ca74ec01d312e8f1446dc4
Jogan Daniel A <jogan.d@tvsnext.com>
2023-08-21 11:20:34 +0530

    [jogan] Add : Funtionality to return 3 fields from rides

--- CabInvoiceGenerator/InvoiceService.cs
+++ CabInvoiceGenerator/InvoiceService.cs
@@ -2,10 +2,25 @@
 {
     public class InvoiceService
     {
-        private readonly int costPerKilometerNormal = 10;
-        private readonly int minimumFareNormal = 5;
-        private readonly int costPerMinuteNormal = 1;
+        private const int COST_PER_KM = 10;
+        private const int MIN_FARE = 5;
+        private  const int COST_PER_MIN = 1;
+        public int totalNumberOfRides = 0;
+        public double totalFare = 0;
+        public double averageFare = 0;
 
+        public int TotalNumberOfRides()
+        {
+            return totalNumberOfRides;
+        }
+        public double TotalFare()
+        {
+            return totalFare;
+        }
+        public double AverageFare()
+        {
+            return averageFare;
+        }
         public int NumberOfRides
         {
             get
@@ -15,21 +30,22 @@
         }
         public double CalculateFare(double distance, double time)
         {
-            double TotalAmount = distance * costPerKilometerNormal + time * costPerMinuteNormal;
-            if (TotalAmount > minimumFareNormal)
+            double TotalAmount = distance * COST_PER_KM + time * COST_PER_MIN;
+            if (TotalAmount > MIN_FARE)
             {
                 return TotalAmount;
             }
-            return minimumFareNormal;
+            return MIN_FARE;
         }
-        public double CalculateFareForRide(Ride ride)
+        public double CalculateFare(Ride[] rides)
         {
-            double TotalAmount = ride.distance * costPerKilometerNormal + ride.time * costPerMinuteNormal;
-            if (TotalAmount > minimumFareNormal)
+            foreach (Ride ride in rides)
             {
-                return TotalAmount;
+                totalFare = ride.distance * COST_PER_KM + ride.time * COST_PER_MIN;
             }
-            return minimumFareNormal;
+            totalNumberOfRides = rides.Length;
+            averageFare = totalFare/ totalNumberOfRides;
+            return averageFare;
         }
     }
 }
--- CabInvoieGeneratorTest/UnitTest1.cs
+++ CabInvoieGeneratorTest/UnitTest1.cs
@@ -17,12 +17,27 @@ namespace CabInvoieGeneratorTest
         [Test]
         public void GivenRides_WhenChecked_ReturnFare()
         {
-            Ride ride = new Ride()
+            Ride[] ride = { new Ride()
             {
                 distance = 10,time = 5
+            }
             };
-            double actual = invoiceservice.CalculateFareForRide(ride);
+            double actual = invoiceservice.CalculateFare(ride);
             double expected = 105;
+            Assert.AreEqual(actual, expected);
+
+        }
+        [Test]
+        public void GivenRides_WhenChecked_ReturnTotalRides()
+        {
+            Ride[] ride = { new Ride()
+            {
+                distance = 10,time = 5
+            }
+            };
+            invoiceservice.CalculateFare(ride);
+            double actual = invoiceservice.totalNumberOfRides;
+            double expected = ride.Length;
             Assert.AreEqual(actual, expected);
 
         }
